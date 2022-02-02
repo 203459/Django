6e92895a754a48ac9c7cbe905b458138cfc3addf
Marcos Shilon <203459@ids.upchiapas.edu.mx>
2022-02-02 10:58:42 -0600

    add creation of environment variables

--- Readme.md
+++ Readme.md
@@ -9,7 +9,19 @@ pip install markdown
 ```bash
 pip install django-filter  
 ```
+```bash
+pip install virtualenv
+```
+```bash
+pip install psycopg2
+```
+```bash
+pip install python-dotenv
+```
+```bash
+pip install python-decouple
+
 ## Agregar la libreria a INSTALLED_APPS en settings
 ```bash
  'rest_framework',
-```
+'rest_framework.authtoken',
--- primerApp/settings.py
+++ primerApp/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nt*xsnmt=d*0_%7^*nk71us8kw87y@)5jhnq3@9cljgb+!cmr8'
+SECRET_KEY = os.getenv ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -90,12 +94,12 @@ WSGI_APPLICATION = 'primerApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME':'dbDjango',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': os.getenv("DB_ENGINE"),
+        'NAME': os.getenv("DB_NAME"),
+        'USER' : os.getenv("DB_USER"),
+        'PASSWORD' : os.getenv("DB_PASSWORD") ,
+        'HOST' : os.getenv("DB_HOST"),
+        'PORT' : os.getenv("DB_PORT")
     }
 }
 
