4ce367892064b51a42d6b5783eefadf5462594cb
Marcos Shilon <203459@ids.upchiapas.edu.mx>
2022-02-07 17:57:38 -0900

    modification in the method POST and PUT

--- loadImage/models.py
+++ loadImage/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 # Create your models here.
 class TablaImage(models.Model):
     name_img = models.CharField(max_length=50, null=True)
-    url_img = models.ImageField(blank='', default="", upload_to='img/')
+    url_img = models.ImageField(blank='', default="", upload_to='assets/img/')
     format_img = models.CharField(max_length=50, null=True)
     created = models.DateTimeField(default=timezone.now)
     editted = models.DateTimeField(blank=True, null=True, default=None)
--- loadImage/views.py
+++ loadImage/views.py
@@ -1,3 +1,4 @@
+from urllib import response
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -26,59 +27,57 @@ class ImageLoad(APIView):
         queryset = TablaImage.objects.all()
         serializer = TerceraTablaSerializer(queryset, many = True, context = {'request':request})
         return Response(self.response_custom("Succes",serializer.data, status = status.HTTP_200_OK))
-        
+  
  def post(self, request):
-     if 'url_img' not in request.data:
-         raise exceptions.ParseError("No se selecciono ningun archivo")
-     archivos =str(request.FILES)
-     serializer = TerceraTablaSerializer(data=request.data)
-     if serializer.is_valid():
-           validated_data = serializer.validated_data
-          ## print(validated_data)
-           #archivo = validated_data['url_img']
-           #archivo.name_img = validated_data['name_img']
-           #archivo.format_img = validated_data['format_img']
-           #validated_data['url_img'] = archivo
-           #validated_data['name_img'] = archivo.name_img
-           #validated_data['format_img'] = archivo.format_img
-            # Convertir y guardar el modelo
-           img = TablaImage(**validated_data)
-           img.save()
+        if 'url_img' not in request.data:
+            raise exceptions.ParseError("No se ha seleccionado un archivo")
 
-           #serializer_response = TerceraTablaSerializer(img)
+        url = request.data['url_img']
+        nombre, formato = os.path.splitext(url.name)
+        request.data['name_img'] = nombre
+        request.data['format_img'] = formato
+        serializer = TerceraTablaSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(self.response_custom("Successful", serializer.data, status = status.HTTP_201_CREATED))
+        return Response(self.response_custom("Error", serializer.errors, status = status.HTTP_400_BAD_REQUEST))
 
-           return Response(self.response_custom("Succes",serializer.data, status = status.HTTP_201_CREATED))
-     return Response(self.response_custom("Errors",serializer.errors,status = status.HTTP_400_BAD_REQUEST))
 class ImageLoadDetail(APIView):
 
-     def get_object(self, pk):
+ def response_custom(self,messages,pay_load, status):
+        responseOk = {"messages":messages,"pay_load":pay_load,"status":status}
+        response1 = json.dumps(responseOk)
+        responseOk2 = json.loads(response1)
+        return responseOk2
+
+ def get_object(self, pk):
             try:
                 return TablaImage.objects.get(pk = pk)
             except TablaImage.DoesNotExist:
                 return 0
 
-     def get(self, request, pk ,format=None):
+ def get(self, request, pk ,format=None):
             idResponse = self.get_object(pk)
             if idResponse != 0:
                 idResponse = TerceraTablaSerializer(idResponse)
                 return Response(idResponse.data, status.HTTP_200_OK)
             return Response("No hay datos", status.HTTP_400_BAD_REQUEST)      
-
-     def put(self, request,pk, format=None):
+ def put(self, request, pk, format = None):
         idResponse = self.get_object(pk)
-        archivos = request.data['url_img']
-        nombre, formato = os.path.splitext(archivos.name)
+        url = request.data['url_img']
+        nombre, formato = os.path.splitext(url.name)
         request.data['name_img'] = nombre
         request.data['format_img'] = formato
         serializer = TerceraTablaSerializer(idResponse, data = request.data)
         if serializer.is_valid():
             serializer.save()
             datas = serializer.data
-            return Response(datas, status = status.HTTP_201_CREATED)
-        return Response(serializer.errors,status = status.HTTP_400_BAD_REQUEST)
-
+            response = self.response_custom("Archivo editado", datas, status = status.HTTP_201_CREATED)
+            return Response(response)
+        response = self.response_custom("No se pudo editar el archivo", serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+        return Response(response)
        
-     def delete(self, request , pk , format=None):
+ def delete(self, request , pk , format=None):
              idResponse = self.get_object(pk)
              if idResponse != 0:
                  idResponse.url_img.delete(save=True)
